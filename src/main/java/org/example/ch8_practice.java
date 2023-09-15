af4a3f72cf90f4cf948f1a08f9ed2a9d23a7d7a5
INWELLCOM\tiffany.lin <tiffany.lin@ennowell.com>
2023-09-15 14:44:56 +0800

    CH8&9 AllDone

--- src/main/java/org/example/ch8_practice.java
+++ src/main/java/org/example/ch8_practice.java
@@ -2,18 +2,46 @@ package org.example;
 
 public class ch8_practice {
     public static void main(String[] args) {
+        //1
+        System.out.println("Q1:");
+        Caaa08 obj = new Caaa08();
+        obj.a = 5; obj.b = 3; obj.c = obj.a* obj.b;
+        System.out.println("a="+obj.a+", b="+obj.b+", c="+obj.c);
+
+        //2
+        System.out.println("\nQ2:");
+        Cbbb08 obj1 = new Cbbb08();
+        Cbbb08 obj2 = new Cbbb08();
+        Cbbb08 avg = new Cbbb08();
+        obj1.x = 5.2; obj1.y = 3.9;
+        obj2.x = 6.5; obj2.y = 4.6;
+        avg.x = (obj1.x+ obj2.x)/2; avg.y = (obj1.y+ obj2.y)/2;
+        System.out.println("obj1.x="+obj1.x+", obj1.y="+obj1.y);
+        System.out.println("obj2.x="+obj2.x+", obj2.y="+obj2.y);
+        System.out.println("avg.x="+avg.x+", avg.y="+avg.y);
+
         //3
+        System.out.println("\nQ3:");
         Cddd student = new Cddd();
         student.BMI("Sandy",1.655,58.2);
 
+
         //5
+        System.out.println("\nQ5:");
         CBox box = new CBox();
         box.length = 1;
         box.width = 1;
         box.height = 1;
         box.showAll();
 
+        //6
+        System.out.println("\nQ6:");
+        Circle08 c1 = new Circle08();
+        c1.radius = 3.0;
+        c1.show_periphery();
+
         //7
+        System.out.println("\nQ7:");
         CTest c = new CTest();
         c.test(3);
         c.test(8);
@@ -21,19 +49,73 @@ public class ch8_practice {
         c.test(18);
         c.test(-3);
 
+        //8
+        CCalculator cCalculator = new CCalculator();
+        cCalculator.set_value(5, 12);
+        cCalculator.show();
+        System.out.println("add="+cCalculator.add());
+        System.out.println("sub="+cCalculator.sub());
+        System.out.println("mul="+cCalculator.mul());
+        System.out.println("avg="+cCalculator.avg());
+
+        //9
+        //不正確, 因為不能設計引數個數和型別完全相同,只有傳回值型別不同的多載
+
+        //10
+        //下列哪個可以呼叫void set(int r)這個函數?
+        //(a) set("Hello");  引數型態不同
+        //(b) set("50");
+        //(c) set(10, 25);   引數個數不同
+        //(b) set(3.14);     引數型態不同
+        // Ans: (b)
+
         //11
+        System.out.println("\nQ11:");
         CWin cWin = new CWin();
         cWin.setWindows(5,3,"my windows");
         cWin.show();
 
+        //12
+        System.out.println("\nQ12:");
+        CCircle08 cCircle08 = new CCircle08();
+        System.out.println("r=2, 圓面積="+cCircle08.area(2));
+        System.out.println("r=2.2f, 圓面積="+cCircle08.area(2.2f));
+        System.out.println("r=2.2, 圓面積="+cCircle08.area(2.2));
+
+
         //15
+        System.out.println("\nQ15:");
         CSphere cs = new CSphere();
         cs.setLocation(3,4,5);
         cs.setRadius(1);
         cs.showAll();
 
+        //16
+        System.out.println("\nQ16:");
+        CData08 tom = new CData08();
+        CData08 mary = new CData08();
+        tom.setName("Tom");
+        tom.setEmail("abc@gmail.com");
+        tom.setBirthday(6,18,2006);
+        tom.show_data();
+        mary.setAll("Mary","def@gmail.com",2,28,1999);
+        mary.show_data();
+
+
+
     }
 }
+
+class Caaa08{
+    int a;
+    int b;
+    int c;
+}   //1
+
+class Cbbb08{
+    double x;
+    double y;
+}   //2
 
 class Cddd{
     String name;
@@ -48,7 +130,7 @@ class Cddd{
         System.out.println(n+"的BMI為"+bmi);
     }
 
-}
+}   //3
 
 class CBox{
     int length;
@@ -71,7 +153,16 @@ class CBox{
         showData();
         System.out.println("體積為:"+volume()+"\n表面積為:"+surfaceArea());
     }
-}
+}   //5
+
+class Circle08{
+    double pi = 3.14;
+    double radius;
+
+    void show_periphery(){
+        System.out.println("圓周長="+2*pi*radius);
+    }
+}   //6
 
 class CTest{
     int num;
@@ -91,7 +182,32 @@ class CTest{
         }
 
     }
-}
+}   //7
+
+class CCalculator{
+    int a;
+    int b;
+
+    void set_value(int a, int b){
+        this.a = a;
+        this.b = b;
+    }
+    void show(){
+        System.out.println("a="+a+", b="+b);
+    }
+    int add(){
+        return a+b;
+    }
+    int sub(){
+        return a-b;
+    }
+    int mul(){
+        return a*b;
+    }
+    double avg(){
+        return (double) add() /2;
+    }
+}   //8
 
 class CWin{
     int width;
@@ -119,7 +235,20 @@ class CWin{
         System.out.println("Name="+name);
         System.out.println("W="+width+", H="+height);
     }
-}
+}   //11
+
+class CCircle08{
+
+    double area(double r){
+        return r*r*3.14;
+    }
+    float area(float r){
+        return (float) (r*r*3.14);
+    }
+    double area(int r){
+        return r*r*3.14;
+    }
+}   //12
 
 class CSphere{
     private int x;
@@ -150,6 +279,50 @@ class CSphere{
     public void showAll(){
         showCenter();
         System.out.println("圓體積為"+volume()+"\n表面積為:"+surfaceArea());
+    }
+
+}   //15
+
+class CData08{
+    private String name;
+    private String email;
+    private int mm;
+    private int dd;
+    private int yy;
+
+    void setName(String n){
+        name = n;
+    }
+    void setEmail(String e){
+        email = e;
+    }
+    void setBirthday(int m, int d, int y){
+        mm = m;
+        dd = d;
+        yy = y;
+        checkDate(m,d,y);
+    }
+    private boolean checkDate(int m, int d, int y){
+        if(m==2 && d>28){return false;}
+        if(d<1 || d>31){return false;}
+        if(y<1900 || y>2099){return false;}
+        else return true;
+    }
+    void setAll(String n, String e, int m, int d, int y){
+        name = n;
+        email = e;
+        mm = m;
+        dd = d;
+        yy = y;
+    }
+    void show_data(){
+        System.out.println("name=" +name);
+        System.out.println("email=" +email);
+        if(!checkDate(mm, dd, yy)){
+            System.out.println("日期格式錯誤");
+        }else{
+            System.out.println(mm+"/"+dd+"/"+yy);
+        }
     }
 
 }
--- src/main/java/org/example/ch9_practice.java
+++ src/main/java/org/example/ch9_practice.java
@@ -3,12 +3,18 @@ package org.example;
 public class ch9_practice {
     public static void main(String[] args) {
         //1
+        System.out.println("Q1:");
         Rectangle r1 = new Rectangle();
         r1.show();
         Rectangle r2 = new Rectangle(2,3);
         r2.show();
+        //2
+        System.out.println("Q2:");
+        Rectangle r3 = new Rectangle();
+        r3.show();
 
         //3
+        System.out.println("\nQ3:");
         Caaa obj1 = new Caaa();
         Caaa obj2 = new Caaa(12);
         System.out.println(obj1.get_value());
@@ -18,17 +24,43 @@ public class ch9_practice {
         System.out.println(obj1.get_value());
         System.out.println(obj2.get_value());
 
+        //4
+        System.out.println("\nQ4:");
+        int[] a4 = {12, 54, 23, 17,90};
+        MinMax minMax = new MinMax(a4);
+        minMax.find_min_max();
+
         //5
+        System.out.println("\nQ5:");
         int[][] a={{12,54,23},{21,12,25}};
         Average aver = new Average(a);
         aver.print_avg();
         aver.print_arr();
 
+        //6
+        System.out.println("\nQ6:");
+        Count count1 = new Count();
+        Count count2 = new Count();
+        Count.show_cnt();
+        Count.setZero();
+        Count.show_cnt();
+        Count.setValue(10);
+        Count.show_cnt();
+
         //7
+        System.out.println("\nQ7:");
         Summation.add(5);
         Summation.add(10);
 
+        //8
+        System.out.println("\nQ8:");
+        Math08 math1 = new Math08();
+        Math08 math2 = new Math08();
+        math1.mypower(2,5);
+        math2.mypower(3,4);
+
         //9
+        System.out.println("\nQ9:");
         int[] a2={-4,190,300,12,-7,8};
         int[] b2={0,2,4,3,6,7};
         MyClip obj = new MyClip(0,255);
@@ -39,6 +71,7 @@ public class ch9_practice {
         obj.print_arr(b2);
 
         //11
+        System.out.println("\nQ11:");
         Fraction f = new Fraction(5,3);
         f.show();
         f.setN(7);
@@ -46,31 +79,48 @@ public class ch9_practice {
         f.show();
         f.setND(100,120);
         f.show();
+        //12
+        //要改寫範例
         //13
+        System.out.println("Q13:");
         Fraction f1 = new Fraction(3,5);
         Fraction f2 = new Fraction(1,4);
         Fraction result = f1.add(f2);
         result.show();
         //14
+        System.out.println("Q14:");
         Fraction f3 = new Fraction(2,3);
         Fraction f4 = new Fraction(1,2);
         Fraction f5;
         f5=Fraction.compare(f3,f4);
         f5.show();
         //15
-
+        System.out.println("Q15:");
+        f3.larger(f4);
 
         //17  //詳解  //題目改自CH9_12
+        System.out.println("\nQ17:");
         Circle[] cir = new Circle[3];
         double[] r={1.0,4.0,2.0};
         for(int i=0;i<cir.length;i++){  //將r的值指派給對應的cir[i]
             cir[i]=new Circle(r[i]);
             cir[i].show();
         }
+        System.out.println("Q16:");
+        System.out.println("avg radius="+Circle.average(cir));
 
-        //19
+        //18
+        System.out.println("\nQ18:");
+        NameCard tom = new NameCard("Tom","here");
+        tom.setPhone("345-7612","0971-666000");
+        tom.show();
+
+        //19&20
+        System.out.println("\nQ19&20:");
         Data stu= new Data("Annie",85,92);
         stu.show();
+
+
 
     }
 }
@@ -84,14 +134,13 @@ class Rectangle{
         height = h;
     }
     public Rectangle() {
-        width = 1;
-        height = 1;
+        this(1,1);
     }
 
     void show(){
-        System.out.println("W="+width+"\nH="+height);
+        System.out.println("W="+width+", H="+height);
     }
-}
+}   //1&2
 
 class Caaa{
     private int value;
@@ -110,7 +159,27 @@ class Caaa{
     int get_value(){
         return value;
     }
-}
+}   //3
+
+class MinMax{
+    private int[] arr;
+
+    public MinMax(int[] a) {
+        arr = a;
+    }
+
+    void find_min_max(){
+        int max = 0, min =arr[0];
+        for(int i=0; i<arr.length; i++){
+            if(arr[i]>max){
+                max = arr[i];
+            } else if (arr[i]<min) {
+                arr[i]=min;
+            }
+        }
+        System.out.println("min="+min+", max="+max);
+    }
+}   //4
 
 class Average{
     private int[][] arr;
@@ -145,7 +214,25 @@ class Average{
 
     }
 
-}
+}   //5
+
+class Count{
+    private static int cnt = 0;
+
+    public Count() {
+        cnt++;
+    }
+
+    public static void setZero(){
+        cnt=0;
+    }
+    public static void setValue(int n){
+        cnt = n;
+    }
+    public static void show_cnt(){
+        System.out.println("建立了"+cnt+"個物件");
+    }
+}   //6
 
 class Summation{
 
@@ -157,7 +244,24 @@ class Summation{
         }
         System.out.println(sum);
     }
-}
+}   //7
+
+class Math08{
+    int pow = 1;  //要設成1, 不然會一直乘與0
+
+    void mypower(int x,int n){
+        if(n == 0){
+            pow = 1;
+            System.out.println(x+"的"+n+"次方 = "+pow);
+        }else{
+            for(int i=1; i<=n; i++){
+                pow*=x;
+            }
+            System.out.println(x+"的"+n+"次方 = "+pow);
+        }
+
+    }
+}   //8
 
 class MyClip{
     private static int low;
@@ -194,7 +298,7 @@ class MyClip{
             System.out.print(i+" ");
         System.out.println();
     }
-}
+}   //9
 
 class Fraction{
     private int num,den;
@@ -231,9 +335,13 @@ class Fraction{
             return f2;
     }
     void larger(Fraction f2){
-
+        if((double)this.num/this.den > (double) f2.num / f2.den){
+            System.out.println("前者大");
+        }else {
+            System.out.println("後者大");
+        }
     }
-}
+}   //11&13&14&15
 
 class Circle{
     private static double pi=3.14;
@@ -242,10 +350,49 @@ class Circle{
     public Circle(double r){       // Circle建構子
         radius=r;
     }
+
     public void show(){
         System.out.printf("area=%6.2f\n",pi*radius*radius);
     }
-}
+    public static double average(Circle[] c){
+        double sum=0.0;
+        for(int i=0; i<c.length; i++){
+            sum+=c[i].radius;
+        }
+        return sum/c.length;
+    }
+}   //16&17
+
+class NameCard{
+    private String name;
+    private String address;
+    private Phone data;
+
+    public NameCard(String na, String ad) {
+        name = na;
+        address = ad;
+        data = new Phone();
+    }
+
+
+    void show(){
+        System.out.println("name="+name);
+        System.out.println("address="+address);
+        System.out.println("tel="+data.tel);
+        System.out.println("mobile="+data.mobile);
+    }
+    void setPhone(String te,String mo){
+        data.tel = te;
+        data.mobile = mo;
+    }
+
+    static class Phone{
+        String tel;
+        String mobile;
+
+
+    }
+}   //18
 
 class Data{
     private String name;
@@ -258,7 +405,10 @@ class Data{
 
     void show(){
         System.out.println("name:"+name+"\neng:"+test.english+"\nmath:"+test.math);
-        System.out.println("avg:"+test.avg());
+        System.out.println("avg:"+avg());
+    }
+    double avg(){
+        return (double) (test.math + test.english) /2;
     }
 
     private class Test{
@@ -270,9 +420,10 @@ class Data{
             math = ma;
         }
 
-        double avg(){
-            return (double) (math + english) /2;
-        }
+//        20題: 把avg()移出去變外部類別Data的函數
+//        double avg(){
+//            return (double) (math + english) /2;
+//        }
 
     }
-}
+}   //19&20
